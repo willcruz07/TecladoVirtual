654819cd20f28817bf28fcaafae68b739c1492e8
Wellen Cruz <lynkan@hotmail.com>
2018-08-14 13:04:53 -0300

    ajuste no javascript

--- app/public/js/envioTelcado.js
+++ app/public/js/envioTelcado.js
@@ -1,5 +1,27 @@
-function preencher(valor) {
-    var campoTexto = document.getElementById("campo");
-    var numeros = campoTexto.value;
-    campoTexto.value = numeros + valor;
-}
+var spice = (function () {
+'use strict'    
+
+    var btn1 =   document.getElementById("1");
+    var btn2 =   document.getElementById("1");
+   
+    function preencher(valor) {
+        var campoTexto = document.getElementById("campo");
+        var numeros = campoTexto.value;        
+        var calc = numeros + valor;
+        campoTexto.value = ("000" + calc).slice(-3);
+    }
+
+    btn1.addEventListener('click', function(e){
+        preencher(e.toElement.value);
+    });
+
+    btn2.addEventListener('click', function(e){
+        preencher(e.toElement.value);
+    });
+
+    return {
+        bolinha : preencher
+    };
+
+})();
+
